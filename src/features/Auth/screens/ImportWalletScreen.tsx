2e57736db6fc4b65b8fc0c6e80e5873dd24e7557
Regin99 <regintmor@gmail.com>
2023-12-29 14:38:46 +0100

    Add bip39 + patch

--- src/features/Auth/screens/ImportWalletScreen.tsx
+++ src/features/Auth/screens/ImportWalletScreen.tsx
@@ -2,6 +2,7 @@ import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import {RootStackScreenProps} from '../../../routes/types';
 import {Screen} from '../../../components/molecules';
+import bip39 from 'react-native-bip39';
 import {BottomSheetModal} from '@gorhom/bottom-sheet';
 import {
   Camera,
@@ -9,6 +10,7 @@ import {
   useCameraPermission,
   useCodeScanner,
 } from 'react-native-vision-camera';
+import {ethers} from 'ethers';
 
 export const ImportWalletScreen = ({
   navigation,
@@ -32,10 +34,29 @@ export const ImportWalletScreen = ({
     },
   });
 
+  const phrase =
+    'rug buyer aim sudden country address dinner tragic tumble ethics trust tattoo';
+
   return (
     <Screen>
       <Button title="Back" onPress={navigation.goBack} />
       <Button title="Open modal" onPress={handleOpenCamera} />
+      <Button
+        title="Validate mnemonic"
+        onPress={() => {
+          const isMnemonicValid = bip39.validateMnemonic(phrase);
+          console.log('isMnemnic valid', isMnemonicValid);
+        }}
+      />
+
+      <Button
+        title="Generate"
+        onPress={() => {
+          const wallet = ethers.Wallet.createRandom();
+          console.log('wallet', wallet);
+        }}
+      />
+
       <BottomSheetModal
         backgroundStyle={{backgroundColor: '#333333'}}
         ref={bottomSheetModalRef}
